63b1bdb9666a5bb3689a17b3a65ca4315e06f3c1
AYUK MA-NDIEP <mandiepayuk4@gmail.com>
2026-04-01 10:00:38 +0100

    admin and student service

--- lingua-webapp/src/app/Services/admin.ts
+++ lingua-webapp/src/app/Services/admin.ts
@@ -1,4 +1,3 @@
-// admin.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError, BehaviorSubject } from 'rxjs';
