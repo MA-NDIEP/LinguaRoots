8080d03f4f62a40285a8c5555f0d51a84e9a7b71
Claudia <claudiatindo95@gmail.com>
2026-04-06 14:00:09 +0100

    Applying basic configurations for deployment

--- lingua-webapp/src/environments/environment.development.ts
+++ lingua-webapp/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // ApiUrl : 'http://localhost:8765'
-  ApiUrl : 'https://api.linguaroots.publicvm.com.'
+  ApiUrl : 'http://localhost:8765'
+  // ApiUrl : 'https://api.linguaroots.publicvm.com.'
 };
