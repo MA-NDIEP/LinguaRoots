30fc8d74044c7d0f1e5d3a65108beeef19304b77
Claudia <claudiatindo95@gmail.com>
2026-04-07 02:35:11 +0100

    Integrating lesson management service with the web application

--- Api Gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
+++ Api Gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.api_gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -26,7 +27,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(csrf -> csrf.disable())
-                .cors(org.springframework.security.config.Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .authorizeExchange(ex -> ex
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll() // allow preflight
                         .anyExchange().permitAll()
