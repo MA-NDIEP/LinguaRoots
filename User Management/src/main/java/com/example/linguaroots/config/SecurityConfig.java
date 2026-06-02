7451d0c99ee90a9fd21f6028f4825737fde117ec
Claudia <claudiatindo95@gmail.com>
2026-06-02 12:18:28 +0100

    Modifications given on last review implemented on alphabet, lessons and posts.

--- User Management/src/main/java/com/example/linguaroots/config/SecurityConfig.java
+++ User Management/src/main/java/com/example/linguaroots/config/SecurityConfig.java
@@ -33,9 +33,9 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/admin/**").hasAnyRole("ADMIN", "ROLE_SUPER_ADMIN")
+                        .requestMatchers("/admin/**").hasAnyRole("ADMIN", "SUPER_ADMIN")
                         .requestMatchers("/superadmin/**").hasRole("SUPER_ADMIN")
-                        .requestMatchers("/student/**").hasAnyRole("ADMIN", "ROLE_SUPER_ADMIN")
+                        .requestMatchers("/student/**").hasAnyRole("ADMIN", "SUPER_ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
